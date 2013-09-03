5141531098a83f7f8eb9a1ecf21db27cfe372c57
mrova <michal.mrowiec@simplesoft.pl>
2013-09-03 17:54:30 +0200

    .

--- src/ZfcUser/Authentication/Adapter/AdapterChain.php
+++ src/ZfcUser/Authentication/Adapter/AdapterChain.php
@@ -12,127 +12,128 @@ use ZfcUser\Exception;
 
 class AdapterChain extends EventProvider implements AdapterInterface
 {
-    /**
-     * @var AdapterChainEvent
-     */
-    protected $event;
+	/**
+	 * @var AdapterChainEvent
+	 */
+	protected $event;
 
-    /**
-     * Returns the authentication result
-     *
-     * @return AuthenticationResult
-     */
-    public function authenticate()
-    {
-        $e = $this->getEvent();
+	/**
+	 * Returns the authentication result
+	 *
+	 * @return AuthenticationResult
+	 */
+	public function authenticate()
+	{
+		$e = $this->getEvent();
 
-        $result = new AuthenticationResult(
-            $e->getCode(),
-            $e->getIdentity(),
-            $e->getMessages()
-        );
+		$result = new AuthenticationResult(
+			$e->getCode(),
+			$e->getIdentity(),
+			$e->getMessages()
+		);
 
-        $this->resetAdapters();
+		$this->resetAdapters();
 
-        return $result;
-    }
+		return $result;
+	}
 
-    /**
-     * prepareForAuthentication
-     *
-     * @param  Request $request
-     * @return Response|bool
-     * @throws Exception\AuthenticationEventException
-     */
-    public function prepareForAuthentication(Request $request)
-    {
-        $e = $this->getEvent();
-        $e->setRequest($request);
+	/**
+	 * prepareForAuthentication
+	 *
+	 * @param  Request $request
+	 * @return Response|bool
+	 * @throws Exception\AuthenticationEventException
+	 */
+	public function prepareForAuthentication(Request $request)
+	{
+		$e = $this->getEvent();
+		$e->setRequest($request);
 
-        $this->getEventManager()->trigger('authenticate.pre', $e);
+		$this->getEventManager()->trigger('authenticate.pre', $e);
 
-        $result = $this->getEventManager()->trigger('authenticate', $e, function($test) {
-            return ($test instanceof Response);
-        });
+		$result = $this->getEventManager()->trigger('authenticate', $e, function($test) {
+			return ($test instanceof Response);
+		});
 
-        if ($result->stopped()) {
-            if($result->last() instanceof Response) {
-                return $result->last();
-            }
 
-            throw new Exception\AuthenticationEventException(
-                sprintf(
-                    'Auth event was stopped without a response. Got "%s" instead',
-                    is_object($result->last()) ? get_class($result->last()) : gettype($result->last())
-                )
-            );
-        }
+		if ($result->stopped()) {
+			if($result->last() instanceof Response) {
+				return $result->last();
+			}
 
-        if ($e->getIdentity()) {
-            return true;
-        }
+// 			throw new Exception\AuthenticationEventException(
+// 				sprintf(
+// 					'Auth event was stopped without a response. Got "%s" instead',
+// 					is_object($result->last()) ? get_class($result->last()) : gettype($result->last())
+// 				)
+// 			);
+		}
 
-        return false;
-    }
+		if ($e->getIdentity()) {
+			return true;
+		}
 
-    /**
-     * resetAdapters
-     *
-     * @return AdapterChain
-     */
-    public function resetAdapters()
-    {
-        $listeners = $this->getEventManager()->getListeners('authenticate');
-        foreach ($listeners as $listener) {
-            $listener = $listener->getCallback();
-            if (is_array($listener) && $listener[0] instanceof ChainableAdapter) {
-                $listener[0]->getStorage()->clear();
-            }
-        }
-        return $this;
-    }
+		return false;
+	}
 
-    /**
-     * logoutAdapters
-     *
-     * @return AdapterChain
-     */
-    public function logoutAdapters()
-    {
-        //Adapters might need to perform additional cleanup after logout
-        $this->getEventManager()->trigger('logout', $this->getEvent());
-    }
+	/**
+	 * resetAdapters
+	 *
+	 * @return AdapterChain
+	 */
+	public function resetAdapters()
+	{
+		$listeners = $this->getEventManager()->getListeners('authenticate');
+		foreach ($listeners as $listener) {
+			$listener = $listener->getCallback();
+			if (is_array($listener) && $listener[0] instanceof ChainableAdapter) {
+				$listener[0]->getStorage()->clear();
+			}
+		}
+		return $this;
+	}
 
-    /**
-     * Get the auth event
-     *
-     * @return AdapterChainEvent
-     */
-    public function getEvent()
-    {
-        if (null === $this->event) {
-            $this->setEvent(new AdapterChainEvent);
-            $this->event->setTarget($this);
-        }
-        return $this->event;
-    }
+	/**
+	 * logoutAdapters
+	 *
+	 * @return AdapterChain
+	 */
+	public function logoutAdapters()
+	{
+		//Adapters might need to perform additional cleanup after logout
+		$this->getEventManager()->trigger('logout', $this->getEvent());
+	}
 
-    /**
-     * Set an event to use during dispatch
-     *
-     * By default, will re-cast to AdapterChainEvent if another event type is provided.
-     *
-     * @param  Event $e
-     * @return AdapterChain
-     */
-    public function setEvent(Event $e)
-    {
-        if (!$e instanceof AdapterChainEvent) {
-            $eventParams = $e->getParams();
-            $e = new AdapterChainEvent();
-            $e->setParams($eventParams);
-        }
-        $this->event = $e;
-        return $this;
-    }
+	/**
+	 * Get the auth event
+	 *
+	 * @return AdapterChainEvent
+	 */
+	public function getEvent()
+	{
+		if (null === $this->event) {
+			$this->setEvent(new AdapterChainEvent);
+			$this->event->setTarget($this);
+		}
+		return $this->event;
+	}
+
+	/**
+	 * Set an event to use during dispatch
+	 *
+	 * By default, will re-cast to AdapterChainEvent if another event type is provided.
+	 *
+	 * @param  Event $e
+	 * @return AdapterChain
+	 */
+	public function setEvent(Event $e)
+	{
+		if (!$e instanceof AdapterChainEvent) {
+			$eventParams = $e->getParams();
+			$e = new AdapterChainEvent();
+			$e->setParams($eventParams);
+		}
+		$this->event = $e;
+		return $this;
+	}
 }
