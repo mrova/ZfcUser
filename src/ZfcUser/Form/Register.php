c819ae7d11b1d003a17a62b54926b39ba9ee515f
mrova <michal.mrowiec@simplesoft.pl>
2013-10-10 16:30:42 +0200

    Signed-off-by: mrova <michal.mrowiec@simplesoft.pl>

--- src/ZfcUser/Form/Register.php
+++ src/ZfcUser/Form/Register.php
@@ -7,60 +7,60 @@ use ZfcUser\Options\RegistrationOptionsInterface;
 
 class Register extends Base
 {
-    protected $captchaElement= null;
+	protected $captchaElement= null;
 
-    /**
-     * @var RegistrationOptionsInterface
-     */
-    protected $registrationOptions;
+	/**
+	 * @var RegistrationOptionsInterface
+	 */
+	protected $registrationOptions;
 
-    /**
-     * @param string|null $name
-     * @param RegistrationOptionsInterface $options
-     */
-    public function __construct($name = null, RegistrationOptionsInterface $options)
-    {
-        $this->setRegistrationOptions($options);
-        parent::__construct($name);
+	/**
+	 * @param string|null $name
+	 * @param RegistrationOptionsInterface $options
+	 */
+	public function __construct($name = null, RegistrationOptionsInterface $options)
+	{
+		$this->setRegistrationOptions($options);
+		parent::__construct($name);
 
-        $this->remove('userId');
-        if (!$this->getRegistrationOptions()->getEnableUsername()) {
-            $this->remove('username');
-        }
-        if (!$this->getRegistrationOptions()->getEnableDisplayName()) {
-            $this->remove('display_name');
-        }
-        if ($this->getRegistrationOptions()->getUseRegistrationFormCaptcha() && $this->captchaElement) {
-            $this->add($this->captchaElement, array('name'=>'captcha'));
-        }
-        $this->get('submit')->setLabel('Register');
-        $this->getEventManager()->trigger('init', $this);
-    }
+		$this->remove('userId');
+		if (!$this->getRegistrationOptions()->getEnableUsername()) {
+			$this->remove('username');
+		}
+		if (!$this->getRegistrationOptions()->getEnableDisplayName()) {
+			$this->remove('display_name');
+		}
+		if ($this->getRegistrationOptions()->getUseRegistrationFormCaptcha() && $this->captchaElement) {
+			$this->add($this->captchaElement, array('name'=>'captcha'));
+		}
+		$this->get('submit')->setLabel('zarejestruj się');
+		$this->getEventManager()->trigger('init', $this);
+	}
 
-    public function setCaptchaElement(Captcha $captchaElement)
-    {
-        $this->captchaElement= $captchaElement;
-    }
+	public function setCaptchaElement(Captcha $captchaElement)
+	{
+		$this->captchaElement= $captchaElement;
+	}
 
-    /**
-     * Set Regsitration Options
-     *
-     * @param RegistrationOptionsInterface $registrationOptions
-     * @return Register
-     */
-    public function setRegistrationOptions(RegistrationOptionsInterface $registrationOptions)
-    {
-        $this->registrationOptions = $registrationOptions;
-        return $this;
-    }
+	/**
+	 * Set Regsitration Options
+	 *
+	 * @param RegistrationOptionsInterface $registrationOptions
+	 * @return Register
+	 */
+	public function setRegistrationOptions(RegistrationOptionsInterface $registrationOptions)
+	{
+		$this->registrationOptions = $registrationOptions;
+		return $this;
+	}
 
-    /**
-     * Get Regsitration Options
-     *
-     * @return RegistrationOptionsInterface
-     */
-    public function getRegistrationOptions()
-    {
-        return $this->registrationOptions;
-    }
+	/**
+	 * Get Regsitration Options
+	 *
+	 * @return RegistrationOptionsInterface
+	 */
+	public function getRegistrationOptions()
+	{
+		return $this->registrationOptions;
+	}
 }
