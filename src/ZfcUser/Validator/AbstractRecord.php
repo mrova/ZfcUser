430af121767d1b81df1d7fda536d8da1659b91ba
mrova <michal.mrowiec@simplesoft.pl>
2013-10-10 19:00:01 +0200

    Signed-off-by: mrova <michal.mrowiec@simplesoft.pl>

--- src/ZfcUser/Validator/AbstractRecord.php
+++ src/ZfcUser/Validator/AbstractRecord.php
@@ -7,112 +7,112 @@ use ZfcUser\Mapper\UserInterface;
 
 abstract class AbstractRecord extends AbstractValidator
 {
-    /**
-     * Error constants
-     */
-    const ERROR_NO_RECORD_FOUND = 'noRecordFound';
-    const ERROR_RECORD_FOUND    = 'recordFound';
+	/**
+	 * Error constants
+	 */
+	const ERROR_NO_RECORD_FOUND = 'noRecordFound';
+	const ERROR_RECORD_FOUND    = 'recordFound';
 
-    /**
-     * @var array Message templates
-     */
-    protected $messageTemplates = array(
-        self::ERROR_NO_RECORD_FOUND => "No record matching the input was found",
-        self::ERROR_RECORD_FOUND    => "A record matching the input was found",
-    );
+	/**
+	 * @var array Message templates
+	 */
+	protected $messageTemplates = array(
+		self::ERROR_NO_RECORD_FOUND => "No record matching the input was found",
+		self::ERROR_RECORD_FOUND    => "Użytkownik istnieje już w naszej bazie. Spróbuj użyć innego maila.",
+	);
 
-    /**
-     * @var UserInterface
-     */
-    protected $mapper;
+	/**
+	 * @var UserInterface
+	 */
+	protected $mapper;
 
-    /**
-     * @var string
-     */
-    protected $key;
+	/**
+	 * @var string
+	 */
+	protected $key;
 
-    /**
-     * Required options are:
-     *  - key     Field to use, 'emial' or 'username'
-     */
-    public function __construct(array $options)
-    {
-        if (!array_key_exists('key', $options)) {
-            throw new Exception\InvalidArgumentException('No key provided');
-        }
+	/**
+	 * Required options are:
+	 *  - key     Field to use, 'emial' or 'username'
+	 */
+	public function __construct(array $options)
+	{
+		if (!array_key_exists('key', $options)) {
+			throw new Exception\InvalidArgumentException('No key provided');
+		}
 
-        $this->setKey($options['key']);
+		$this->setKey($options['key']);
 
-        parent::__construct($options);
-    }
+		parent::__construct($options);
+	}
 
-    /**
-     * getMapper
-     *
-     * @return UserInterface
-     */
-    public function getMapper()
-    {
-        return $this->mapper;
-    }
+	/**
+	 * getMapper
+	 *
+	 * @return UserInterface
+	 */
+	public function getMapper()
+	{
+		return $this->mapper;
+	}
 
-    /**
-     * setMapper
-     *
-     * @param UserInterface $mapper
-     * @return AbstractRecord
-     */
-    public function setMapper(UserInterface $mapper)
-    {
-        $this->mapper = $mapper;
-        return $this;
-    }
+	/**
+	 * setMapper
+	 *
+	 * @param UserInterface $mapper
+	 * @return AbstractRecord
+	 */
+	public function setMapper(UserInterface $mapper)
+	{
+		$this->mapper = $mapper;
+		return $this;
+	}
 
-    /**
-     * Get key.
-     *
-     * @return string
-     */
-    public function getKey()
-    {
-        return $this->key;
-    }
+	/**
+	 * Get key.
+	 *
+	 * @return string
+	 */
+	public function getKey()
+	{
+		return $this->key;
+	}
 
-    /**
-     * Set key.
-     *
-     * @param string $key
-     */
-    public function setKey($key)
-    {
-        $this->key = $key;
-        return $this;
-    }
+	/**
+	 * Set key.
+	 *
+	 * @param string $key
+	 */
+	public function setKey($key)
+	{
+		$this->key = $key;
+		return $this;
+	}
 
-    /**
-     * Grab the user from the mapper
-     *
-     * @param string $value
-     * @return mixed
-     */
-    protected function query($value)
-    {
-        $result = false;
+	/**
+	 * Grab the user from the mapper
+	 *
+	 * @param string $value
+	 * @return mixed
+	 */
+	protected function query($value)
+	{
+		$result = false;
 
-        switch ($this->getKey()) {
-            case 'email':
-                $result = $this->getMapper()->findByEmail($value);
-                break;
+		switch ($this->getKey()) {
+			case 'email':
+				$result = $this->getMapper()->findByEmail($value);
+				break;
 
-            case 'username':
-                $result = $this->getMapper()->findByUsername($value);
-                break;
+			case 'username':
+				$result = $this->getMapper()->findByUsername($value);
+				break;
 
-            default:
-                throw new \Exception('Invalid key used in ZfcUser validator');
-                break;
-        }
+			default:
+				throw new \Exception('Invalid key used in ZfcUser validator');
+				break;
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 }
